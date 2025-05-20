2ef9c568fb1657693e063468077d7e43e9c6a6ef
N Nisha <nishan.mafoi@gmail.com>
2025-05-20 14:46:02 +0530

    mvc

---
+++ may19/WebApplication3/WebApplication3/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApplication3.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Location { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+    }
+    
+
+
+    
+}
---
+++ may19/WebApplication4/WebApplication4/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+
+namespace WebApplication4.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        
+        public decimal Price { get; set; }
+        
+        public string ProductDescription { get; set; }
+    }
+    
+}

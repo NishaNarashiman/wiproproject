56d2c22690997fdaed01e6716ef2924c74229d59
N Nisha <nishan.mafoi@gmail.com>
2025-05-29 11:57:08 +0530

    Emp system

---
+++ may29/WebApplication9/WebApplication9/Models/Contact.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication9.Models
+{
+    public class Contact
+    {
+        public int Name { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+
+    }
+}

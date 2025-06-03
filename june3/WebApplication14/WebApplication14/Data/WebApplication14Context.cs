5e5e026313583a6263d3a0010cb0a96baca1909c
N Nisha <nishan.mafoi@gmail.com>
2025-06-03 13:44:00 +0530

    web api

---
+++ june3/WebApplication14/WebApplication14/Data/WebApplication14Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication14.Models;
+
+namespace WebApplication14.Data
+{
+    public class WebApplication14Context : DbContext
+    {
+        public WebApplication14Context (DbContextOptions<WebApplication14Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplication14.Models.Product> Product { get; set; } = default!;
+    }
+}
---
+++ june3/WebApplication14/WebApplication14/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication14.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string   ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public decimal ProductPrice { get; set; }
+        public decimal ProductQuantity { get; set; }
+
+        
+    }
+}
